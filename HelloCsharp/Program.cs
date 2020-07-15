dd8cdc1e9473d1f07e707a046aba74de2bb4b0c5
carlos chavez <cvchavez92@gmail.com>
2020-07-15 09:48:14 -0600

    added comments to Program.cs

--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿// using System; using is not an importer
+// or
+using c = System.Console; // alias
 
-namespace HelloCsharp
+namespace HelloCsharp // directory, by convention you want your namespace to match your project structure
+
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // PascalCasing, Main is keyword, main is not
         {
-            Console.WriteLine("Hello World!");
+            c.WriteLine("hello Carlos"); // fully qualified name
+            HelloCsharp.Program.Test();
+        }
+        static void Test(){
+          c.WriteLine("test method");
         }
     }
 }
+
+// namespace System{
+//   class Console{
+//     void WriteLine(){
+
+//     }
+//   }
+// }
