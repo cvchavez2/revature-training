8d525f25b65c04b6d32af733a56b60297a623d04
carlos chavez <cvchavez92@gmail.com>
2020-07-15 14:36:36 -0600

    added calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Calculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to our new calculator");
+      var stay = true;
+      do
+      {
+        System.Console.WriteLine("Menu");
+        System.Console.WriteLine("1 for +");
+        System.Console.WriteLine("2 for -");
+        System.Console.WriteLine("3 for *");
+        System.Console.WriteLine("4 for /");
+        System.Console.Write("enter operation: ");
+        string operation = Console.ReadLine();
+
+        double result = 0;
+        switch (operation)
+        {
+          case "1":
+            result = GetNum1() + GetNum2();
+            break;
+          case "2":
+            result = GetNum1() - GetNum2();
+            break;
+          case "3":
+            result = GetNum1() * GetNum2();
+            break;
+          case "4":
+            result = GetNum1() / GetNum2();
+            break;
+          default:
+            stay = false;
+            break;
+        }
+        System.Console.WriteLine("result: {0} \n", result);
+      } while (stay);
+    }
+    static double GetNum1()
+    {
+      System.Console.Write("enter first num: ");
+      var num1 = 1D;
+      if (double.TryParse(Console.ReadLine(), out num1))
+      { // tryparse will return a boolean
+        System.Console.WriteLine("valid");
+      }
+      else
+      {
+        num1 = -1D;
+        System.Console.WriteLine("digit not valid, providing default {0}", num1);
+      }
+      return num1;
+    }
+    static double GetNum2()
+    {
+      var num2 = -1D;
+      System.Console.Write("enter second num: ");
+      if (double.TryParse(Console.ReadLine(), out num2))
+      {
+        System.Console.WriteLine("valid");
+      }
+      else
+      {
+        num2 = -1D;
+        System.Console.WriteLine("digit not valid, providing default {0}", num2);
+      }
+      return num2;
+    }
+  }
+}
--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -1,22 +1,38 @@
-﻿// using System; using is not an importer
-// or
-using c = System.Console; // alias
+﻿using C = System.Console;
 
-namespace HelloCsharp // directory, by convention you want your namespace to match your project structure
-
-{
-    class Program
-    {
-        static void Main(string[] args) // PascalCasing, Main is keyword, main is not
-        {
-            c.WriteLine("hello Carlos"); // fully qualified name
-            HelloCsharp.Program.Test();
-        }
-        static void Test(){
-          c.WriteLine("test method");
-        }
+namespace HelloCsharp{
+  class Program{ // good practice is to match the name with the name of the file - Program as for Program.cs
+    static void Main(string [] args){
+      C.Write("Enter your name: ");
+      var name = C.ReadLine();  // lazy instantiation 
+      C.Write("Enter last name: ");
+      string input = C.ReadLine(); // eager, datatype is explicit prior to content of Readline
+      C.WriteLine("name entered is: " + name);
+      C.WriteLine("{0} {1}", name, input);
+      C.WriteLine($"{name} is my name, This, {input}, my last name."); // interpolation
     }
+  }
 }
+// ///////////////////////////////////////////////////////////////////////////
+// using System; using is not an importer
+// or
+// using c = System.Console; // alias
+
+// namespace HelloCsharp // directory, by convention you want your namespace to match your project structure
+
+// {
+//     class Program
+//     {
+//         static void Main(string[] args) // PascalCasing, Main is keyword, main is not
+//         {
+//             c.WriteLine("hello Carlos"); // fully qualified name
+//             HelloCsharp.Program.Test();
+//         }
+//         static void Test(){
+//           c.WriteLine("test method");
+//         }
+//     }
+// }
 
 // namespace System{
 //   class Console{
